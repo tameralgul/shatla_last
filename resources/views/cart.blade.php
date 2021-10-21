96ce0e9bc528568d2d42dfcdbcb51f32b0e21378
tameralgul <tameralgul@live.com>
2021-10-22 00:23:26 +0300

    integrate with tap payment

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,5 +1,9 @@
 @extends('front_layout.layout')
 @section('content')
+
+    <div id="pageRoot"></div>
+
+
 <div class="row">
     <div class="col-sm-8 content-box ">
         <div class="row my-cart-box">
@@ -72,7 +76,7 @@
         </div>
     </div>
     <div class="col-sm-3">
-        <form method="get" action="/payment" class="row order-box">
+        <div class="row order-box">
             <div class="col-sm-12">
                 <div class="row ">
                     <div class="col-sm-12 order-word text-right">
@@ -94,7 +98,6 @@
                         {{ $total ."" }}  
                             </span>
                         <span class="nis-result">  رس</span>
-
                     </div>
                 </div>
             </div>
@@ -120,71 +123,18 @@
                         <span class="price-result">{{  $total-$discount  }}</span>
                         <span class="nis-result"> رس</span>
                     </div>
-                    <input type="text" style="display: none;" value="{{$total-$discount}}" name="total">
                 </div>
             </div>
-            <button class="col-sm-12 btn btn-checkout mb-3 font-weight-bold ">
-                تأكيد الطلب
+			<button id="openLightBox" class="col-sm-12 btn btn-checkout mb-3 font-weight-bold " onclick="goSell.openLightBox()">
+                تأكيد الطلب	
             </button>
 
-        </form>
-
-    </div>
-</div>
-</div>
-</div>
-
-{{-- <div class="col-sm-3">
-        <div class="row order-box">
-            <div class="col-sm-12">
-                <div class="row ">
-                    <div class="col-sm-12 order-word text-right">
-                        ملخص الطلب
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-sm-12">
-                <div class="row total-box">
-                    <div class="col-sm-6 col-6 total text-right">
-                        مجموع الطلب
-                    </div>
-                    <div class="col-sm-6 col-6">
-                        <span class="price-result">{{ $total->product->price }}</span>
-<span class="nis-result"> رس</span>
-</div>
-</div>
-</div>
-<div class="col-sm-12">
-    <div class="row shipping-box">
-        <div class="col-sm-6 col-6 shipping text-right">
-            الخصم </div>
-        <div class="col-sm-6 col-6">
-            <span class="price-result">{{ $total->product->discount->value }}</span>
-            <span class="nis-result"> رس</span>
         </div>
+
     </div>
 </div>
 
-<hr class="col-sm-10 line-hr" style="margin-bottom: 1em">
-<div class="col-sm-12">
-    <div class="row mb-3">
-        <div class="col-sm-6 col-6 shipping text-right">
-            المجموع الكلي </div>
-        <div class="col-sm-6 col-6">
-            <span class="price-result">{{  $total->product->price - $total->product->discount->value  }}</span>
-            <span class="nis-result"> رس</span>
-        </div>
-    </div>
-</div>
-<button class="col-sm-12 btn btn-checkout ">
-    تأكيد الطلب
-</button>
-
-</div>
-
-</div>
-</div> --}}
+    <div id="pageRoot"></div>
 
 @endsection
 @push('css')
@@ -211,7 +161,120 @@
 @endpush
 @section('js')
     <script src="{{asset('assets/js/sweetalert.js')}}"></script>
-<script type="text/javascript">
+
+    <script type="text/javascript">
+	goSell.config({
+  containerID: "pageRoot",
+  gateway: {
+    publicKey: "pk_test_dROwugnX02QMLCBaSt7zY85y",
+    merchantId: null,
+    language: "en",
+    contactInfo: true,
+    supportedCurrencies: "all",
+    supportedPaymentMethods: "all",
+    saveCardOption: false,
+    customerCards: true,
+    notifications: "standard",
+    callback: (response) => {
+      console.log("response", response);
+    },
+    onClose: () => {
+      console.log("onClose Event");
+    },
+    backgroundImg: {
+      url: "imgURL",
+      opacity: "0.5",
+    },
+    labels: {
+      cardNumber: "Card Number",
+      expirationDate: "MM/YY",
+      cvv: "CVV",
+      cardHolder: "Name on Card",
+      actionButton: "Pay",
+    },
+    style: {
+      base: {
+        color: "#535353",
+        lineHeight: "18px",
+        fontFamily: "sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "rgba(0, 0, 0, 0.26)",
+          fontSize: "15px",
+        },
+      },
+      invalid: {
+        color: "red",
+        iconColor: "#fa755a ",
+      },
+    },
+  },
+  customer: {
+    id: null,
+    first_name: "First Name",
+    middle_name: "Middle Name",
+    last_name: "Last Name",
+    email: "demo@email.com",
+    phone: {
+      country_code: "965",
+      number: "99999999",
+    },
+  },
+  order: {
+    amount: <?php 
+		$x = $total-$discount;
+		echo $x ; 
+	?>,
+    currency: "SAR",
+    items: [
+	<?php 
+		foreach(session('cart') as $id => $details){
+			$t =  $details['available_in_stock'] * (((100 - $details['product_discount_id']) * $details['price'])/100);
+			echo '{
+				id: 1,
+				name: "'.$details['title'].'",
+				description: "",
+				quantity: "'.$details['available_in_stock'].'",
+				amount_per_unit: "'.$details['price'].'",
+				discount: {
+				  type: "P",
+				  value: "'.$details['product_discount_id'].'%",
+				},
+				total_amount: "'.$t.'",
+			},';
+		}
+	?>
+    ],
+    shipping: null,
+    taxes: null,
+  },
+  transaction: {
+    mode: "charge",
+    charge: {
+      saveCard: false,
+      threeDSecure: true,
+      description: "فاتورة شراء",
+      statement_descriptor: "Sample",
+      reference: {
+        transaction: "txn_0001", //here you should put transaction id 
+        order: "ord_0001", //here you should put order id 
+      },
+      hashstring:"",
+      metadata: {},
+      receipt: {
+        email: false,
+        sms: true,
+      },
+      redirect: "https://shtlah.com/cart", //here you should type url to receive payment response.
+      post: null,
+    },
+  },
+});
+
+	</script>
+
+	<script type="text/javascript">
     $(".update-cart").change(function (e) {
             e.preventDefault();
        
